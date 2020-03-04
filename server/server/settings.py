ccfb9e737b85bdd98afa8d87fc48b1e54f49de7c
sankhadip samanta <sankhsamanta.cse17@chitkarauniversity.edu.in>
2020-03-04 11:13:37 +0530

    feat: user app, auth-token url added

--- server/server/settings.py
+++ server/server/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
@@ -97,6 +98,7 @@ REST_FRAMEWORK = {
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ),
 }
 
