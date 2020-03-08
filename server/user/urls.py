7a2981996526b5d7c898522e0af3b7379cb67d13
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-08 21:56:01 +0530

    feat: knox logout code added

--- server/user/urls.py
+++ server/user/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from .views import RegistrationView
-
+from knox import views as KnowViews
 
 
 urlpatterns=[
     path('api/auth', include('knox.urls')),
-    path('api/auth/signup', RegistrationView.as_view())
+    path('api/auth/signup', RegistrationView.as_view()),
+    path('api/auth/logout', KnowViews.LogoutView().as_view(),name='knox_logout')
 ]
