12f9191321e1ce34a47b68e4967ae1149f58e7f9
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 23:19:14 +0530

    feat: validator.ts module updated to bed used dynamically in any component

--- client/src/validators/validator.ts
+++ client/src/validators/validator.ts
@@ -1,11 +1,45 @@
-class Validator {
-    public requiredOnBlur(e: any): boolean {
-        return (e.target['value'].length > 0 ? true : false)
+interface IField {
+    [propName: string]: any;
+}
+
+
+export class Validator {
+
+    public fields: IField;
+
+    constructor(fields: IField) {
+        this.fields = fields;
+
+        for (let key in this.fields) {
+            this.fields[key]=' ';
+            Object.defineProperty(this, key, {
+                get: () => {
+                    // return this.fields != null ? this.fields[key] : null
+                    return this.fields[key];
+                },
+                set:(value:any)=>
+                {
+                    // if(this.fields!=null)
+                    // {
+                    //     this.fields[key]=value;
+                    // }
+                    this.fields[key]=value;
+                }
+            })
+            // Object.defineProperty(this,`${key}.touched`,{
+            //     value:null
+            // })
+        }
+    }
+    public requiredOnBlur(e: any): void {
+        // return (e.target['value'].length > 0 ? true : false)
+        this.fields[e.target.name]=e.target.value;
     }
 
-    public requiredOnChnage(e: any): boolean {
-        return (e.target['value'].length > 0 ? true : false)
+    public requiredOnChnage(e: any): void {
+        // return (e.target['value'].length > 0 ? true : false)
+        this.fields[e.target.name]=e.target.value;
     }
 }
 
-export const validator = new Validator();
+// export const validator = new Validator(null);
