0860e6d6851d31b0efd4b750fa6e313655aab628
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 23:19:56 +0530

    feat: signup.tsx component updated according to validator.ts module

--- client/src/components/auth/signup.tsx
+++ client/src/components/auth/signup.tsx
@@ -1,9 +1,11 @@
-import React, { useState, Fragment } from 'react';
+import React, { useState, Fragment, useEffect } from 'react';
 import { Grid, Form, Segment, Button, Message } from 'semantic-ui-react';
 
 import Axios from '../../axios/axios';
 import { HeaderComponent } from './AuthHeader';
-import { validator } from '../../validators/validator';
+import { Validator } from '../../validators/validator';
+
+let SignupForm = new Validator({ username: "", email: "", password: "" });
 
 export const Signup = (props: any) => {
     interface IUser {
@@ -21,9 +23,11 @@ export const Signup = (props: any) => {
     }
 
     const [user, setUser] = useState<IUser>({ username: '', email: '', password: '' });
-    const [error, setError]=useState<ErrorUser>({ username:true, email:true, password:true });
+    const [force, setForce] = useState(0);
+    // const [error, setError] = useState<ErrorUser>({ username: true, email: true, password: true });
 
     const submit: submitFunc = async (e: React.FormEvent<HTMLFormElement>) => {
+        console.log(SignupForm)
         const { username, email, password } = user;
         e.preventDefault();
         try {
@@ -49,30 +53,54 @@ export const Signup = (props: any) => {
                                     <input
                                         placeholder="Enter your username"
                                         value={user?.username}
+                                        name="username"
                                         onChange={(e) => {
                                             setUser({ ...user, username: e.target.value });
-                                            setError({ ...error,username:validator.requiredOnChnage(e) })
+                                            // setError({ ...error, username: validator.requiredOnChnage(e) })
+                                            SignupForm.requiredOnChnage(e);
                                         }}
-                                        onBlur={(e) => { setError({ ...error, username:validator.requiredOnBlur(e) }); }} />
+                                        // onBlur={(e) => { setError({ ...error, username: validator.requiredOnBlur(e) }); }}
+                                        onBlur={(e) => {
+                                            setForce(force => force + 1);
+                                            SignupForm.requiredOnBlur(e);
+                                        }}
+                                    />
                                 </Form.Field>
-                                { !error.username ? <Message color="red">Username is required</Message> : null}
+                                {/* {!error.username ? <Message color="red">Username is required</Message> : null} */}
+                                {console.log(SignupForm.fields['username'].length)}
+                                {SignupForm.fields['username'].length === 0 ? <Message color="red">Username is required</Message> : null}
                                 <Form.Field>
                                     <label>Email</label>
                                     <input
                                         placeholder="Enter your email"
                                         value={user?.email}
+                                        name="email"
                                         onChange={(e) => {
                                             setUser({ ...user, email: e.target.value });
-                                        }} />
+                                            SignupForm.requiredOnChnage(e);
+                                        }}
+                                        onBlur={(e) => {
+                                            setForce(force => force + 1);
+                                            SignupForm.requiredOnBlur(e);
+                                        }}
+                                    />
                                 </Form.Field>
+                                {SignupForm.fields['email'].length === 0 ? <Message color="red">Email is required</Message> : null}
                                 <Form.Field>
                                     <label>Password</label>
                                     <input
                                         placeholder="Enter your password"
                                         value={user?.password}
+                                        name="password"
                                         onChange={(e) => {
-                                            setUser({ ...user, password: e.target.value })
-                                        }} />
+                                            setUser({ ...user, password: e.target.value });
+                                            SignupForm.requiredOnChnage(e);
+                                        }}
+                                        onBlur={(e) => {
+                                            setForce(force => force + 1);
+                                            SignupForm.requiredOnBlur(e);
+                                        }}
+                                    />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Confirm Password</label>
