4cde38ececfb4f3ceb836ff621d8ed4af36c540f
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 19:28:10 +0530

    feat: interface reaarranged, submit func added

--- client/src/components/auth/signup.tsx
+++ client/src/components/auth/signup.tsx
@@ -1,22 +1,35 @@
-import React,{ useState, Fragment } from 'react';
+import React, { useState, Fragment } from 'react';
 import { Grid, Form, Segment, Button } from 'semantic-ui-react';
 
 import Axios from '../../axios/axios';
 import { HeaderComponent } from './AuthHeader';
 
 
-export const Signup=(props:any)=>
-{
-    interface IUser{
-        username:string;
-        email:string;
-        password:string;
+export const Signup = (props: any) => {
+    interface IUser {
+        username: string;
+        email: string;
+        password: string;
     }
-    interface InputFunc{
-        (e:React.ChangeEvent<HTMLInputElement>, setter:any):void
+    interface submitFunc {
+        (e: React.FormEvent<HTMLFormElement>): void
     }
-    const [ user, setUser ]=useState<IUser | null>(null);
-    return(
+
+    const [user, setUser] = useState<IUser>({ username: '', email: '', password: '' });
+
+    const submit: submitFunc = async (e: React.FormEvent<HTMLFormElement>) => {
+        const { username, email, password } = user;
+        e.preventDefault();
+        try {
+            const res = await Axios.post('/api/auth/signup', { username, email, password });
+            console.log(res);
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
+
+    return (
         <Fragment>
             <Grid columns={3} divided>
                 <Grid.Row style={{ marginTop: 50 }}>
@@ -24,18 +37,18 @@ export const Signup=(props:any)=>
                     <Grid.Column>
                         <Segment raised>
                             <HeaderComponent name="Signup" />
-                            <Form>
+                            <Form onSubmit={submit}>
                                 <Form.Field>
                                     <label>Username</label>
-                                    <input placeholder="Enter your username" value={user?.username} />
+                                    <input placeholder="Enter your username" value={user?.username} onChange={(e) => { setUser({ ...user, username: e.target.value }) }} />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Email</label>
-                                    <input placeholder="Enter your email" value={user?.email} />
+                                    <input placeholder="Enter your email" value={user?.email} onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Password</label>
-                                    <input placeholder="Enter your password" value={user?.password} />
+                                    <input placeholder="Enter your password" value={user?.password} onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Confirm Password</label>
