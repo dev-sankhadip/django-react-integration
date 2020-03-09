bc7bc290a2985da84f2ebfe7d5c0ae360a903572
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 09:58:31 +0530

    feat: login component added

---
+++ client/src/components/auth/login.tsx
@@ -0,0 +1,15 @@
+import React, { useState, Fragment } from 'react';
+
+interface LoginProps {
+    name: string;
+    email: string
+}
+
+
+export const Login = () => {
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
