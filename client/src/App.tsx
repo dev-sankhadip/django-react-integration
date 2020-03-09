b4627da6eb66c57549a9193b1edcb29f16b4ed7f
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 09:59:21 +0530

    upadte

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
-import './App.css';
+import { Switch, BrowserRouter, Route } from 'react-router-dom';
+import { Login } from './components/auth/login';
+import { Signup } from './components/auth/signup';
+
+
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
