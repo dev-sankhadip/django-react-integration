ccfdfb948ad65630d97414ff2bc2fbef544188de
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-09 22:07:21 +0530

    feat: validation added with own reusable module

--- client/src/components/auth/signup.tsx
+++ client/src/components/auth/signup.tsx
@@ -1,9 +1,9 @@
 import React, { useState, Fragment } from 'react';
-import { Grid, Form, Segment, Button } from 'semantic-ui-react';
+import { Grid, Form, Segment, Button, Message } from 'semantic-ui-react';
 
 import Axios from '../../axios/axios';
 import { HeaderComponent } from './AuthHeader';
-
+import { validator } from '../../validators/validator';
 
 export const Signup = (props: any) => {
     interface IUser {
@@ -11,11 +11,17 @@ export const Signup = (props: any) => {
         email: string;
         password: string;
     }
+    interface ErrorUser {
+        username?: boolean;
+        email?: boolean;
+        password?: boolean;
+    }
     interface submitFunc {
         (e: React.FormEvent<HTMLFormElement>): void
     }
 
     const [user, setUser] = useState<IUser>({ username: '', email: '', password: '' });
+    const [error, setError]=useState<ErrorUser>({ username:true, email:true, password:true });
 
     const submit: submitFunc = async (e: React.FormEvent<HTMLFormElement>) => {
         const { username, email, password } = user;
@@ -40,15 +46,33 @@ export const Signup = (props: any) => {
                             <Form onSubmit={submit}>
                                 <Form.Field>
                                     <label>Username</label>
-                                    <input placeholder="Enter your username" value={user?.username} onChange={(e) => { setUser({ ...user, username: e.target.value }) }} />
+                                    <input
+                                        placeholder="Enter your username"
+                                        value={user?.username}
+                                        onChange={(e) => {
+                                            setUser({ ...user, username: e.target.value });
+                                            setError({ ...error,username:validator.requiredOnChnage(e) })
+                                        }}
+                                        onBlur={(e) => { setError({ ...error, username:validator.requiredOnBlur(e) }); }} />
                                 </Form.Field>
+                                { !error.username ? <Message color="red">Username is required</Message> : null}
                                 <Form.Field>
                                     <label>Email</label>
-                                    <input placeholder="Enter your email" value={user?.email} onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
+                                    <input
+                                        placeholder="Enter your email"
+                                        value={user?.email}
+                                        onChange={(e) => {
+                                            setUser({ ...user, email: e.target.value });
+                                        }} />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Password</label>
-                                    <input placeholder="Enter your password" value={user?.password} onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
+                                    <input
+                                        placeholder="Enter your password"
+                                        value={user?.password}
+                                        onChange={(e) => {
+                                            setUser({ ...user, password: e.target.value })
+                                        }} />
                                 </Form.Field>
                                 <Form.Field>
                                     <label>Confirm Password</label>
