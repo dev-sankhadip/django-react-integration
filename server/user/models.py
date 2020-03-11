04f1b87da2e63b848f21ec94ab1479c411021cac
Sankhadip Samanta <sankhadip.2000@gmail.com>
2020-03-11 15:39:34 +0530

    feat:(server) models updated( email column made unique )

--- server/user/models.py
+++ server/user/models.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+User._meta.get_field('email')._unique = True
+
 # class Profile:
 #     userid=models.OneToOneField(User, on_delete=models.CASCADE)
 #     phone=models.OneToOneField(User, on_delete=models.CASCADE)
